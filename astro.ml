15e1b5c47a352a6fbd880857e8158b05e8c5b7e1
Zbadis <zakaria.badis@yahoo.fr>
2014-05-15 23:50:21 +0200

    Update astro.ml

--- astro.ml
+++ astro.ml
@@ -10,15 +10,16 @@ Graphics.set_window_title "Astroml-BZ :)";;
 Graphics.set_color (Graphics.rgb 255 255 255);;
 Graphics.fill_rect 0 0 500 500;
 
-(* La bannière de score et autonomie*)
+(* La bannière de l'aide*)
 Graphics.set_color (Graphics.rgb 200 173 127);;
-Graphics.moveto 50 520;;
 Graphics.fill_rect 0 500 500 50;;
 Graphics.set_color (Graphics.rgb 255 255 255);;
-
-Graphics.draw_string "Score :  ";;(*Prochainement afficher le score réel*)
 Graphics.moveto 300 520;;
-Graphics.draw_string "Autonomie :  ";; (*Prochainement afficher l'autonomie réel*)
+Graphics.draw_string " Bonne partie :-) ";;
+Graphics.moveto 20 530;;
+Graphics.draw_string "Aide : ramassez toutes les etoiles ";; 
+Graphics.moveto 20 515;;
+Graphics.draw_string "       sans epuiser votre batterie ";; 
 
 (*recolorer la fenetre principale *)
 Graphics.set_color (Graphics.rgb 0 0 0);;
@@ -26,8 +27,8 @@ Graphics.set_color (Graphics.rgb 0 0 0);;
 (*Initialisation du score *)
 let score = ref 0;;
 
-(*Initialisation du niveau d'autonomie 50% *)
-let autonomie = ref 0;;
+(*Initialisation du niveau d'autonomie 45% *)
+let autonomie = ref 45;;
 
 (* ************************************** *)
 let f i =
@@ -44,9 +45,9 @@ let afficher img (i,j) =
   Dessiner.dessiner_image img (!j * 50) (300 - !i * 50);;
 
 (* Position des batteries*)
-let batteries =  [(ref 6,ref 9);(ref 6,ref 0);(ref (-3),ref 0);(ref (-3),ref 9)];;
+let batteries =  [(ref 6,ref 9);(ref 6,ref 0);(ref (-3),ref 0);(ref (-3),ref 9);(ref 1,ref 0);];;
 (* étoiles*)
-let etoiles =  [(ref 1,ref 0);(ref 2,ref 0);(ref 3,ref 0);(ref 4,ref 0);
+let etoiles =  [(ref (-3),ref 5);(ref 2,ref 0);(ref 3,ref 0);(ref 4,ref 0);
                  (ref 0,ref 9);(ref 1,ref 9);(ref 2,ref 9);(ref 3,ref 9);
                  (ref (-3),ref 3);(ref (-3),ref 4);
                  (ref 4,ref 4);(ref 5,ref 4)];;
@@ -69,17 +70,17 @@ let afficher_mobiles () =
 
 let deplacer direction =
   let () = match direction with
-    | E -> if(!perso_j + 1 < 10) then perso_j := !perso_j + 1
-    | O -> if(!perso_j - 1 > -1) then perso_j := !perso_j - 1  
-    | S -> if(!perso_i + 1 < 7) then  perso_i := !perso_i + 1
-    | N -> if(!perso_i - 1 > -4) then  perso_i := !perso_i - 1
+    | E -> if(!perso_j + 1 < 10) then perso_j := !perso_j + 1  ; autonomie := !autonomie - 5; 
+    | O -> if(!perso_j - 1 > -1) then perso_j := !perso_j - 1  ; autonomie := !autonomie - 5; 
+    | S -> if(!perso_i + 1 < 7) then  perso_i := !perso_i + 1  ; autonomie := !autonomie - 5; 
+    | N -> if(!perso_i - 1 > -4) then  perso_i := !perso_i - 1 ; autonomie := !autonomie - 5; 
   in
   let ramasser_batterie (bi,bj) =
     if (!bi,!bj) = (!perso_i,!perso_j) then
       begin
-        autonomie := !autonomie + 20; 
-        bi := 4;
-        bj := 5;
+        autonomie := !autonomie + 50; 
+        bi := -15;
+        bj := 15;
       end
   in
 
@@ -87,10 +88,9 @@ let deplacer direction =
   let ramasser_etoile (ei,ej) =
     if (!ei,!ej) = (!perso_i,!perso_j) then
       begin
-        score := !score + 1;
-        autonomie := !autonomie - 10; 
-        ei := 0;
-        ej := 4;
+        score := !score + 10;
+	ei := -15;
+        ej := 15;
       end
   in
 
@@ -103,13 +103,33 @@ let deplacer direction =
     	| O -> perso_j := !perso_j + 1  
     	| S -> perso_i := !perso_i - 1
     	| N -> perso_i := !perso_i + 1
-        ;
-        autonomie := !autonomie - 10;
       end
   in
   List.iter ramasser_batterie batteries;
   List.iter ramasser_etoile etoiles;
   List.iter toucher_rocher rochers;
+
+  (* tester si le niveau d'autonmie n'est pas épuisé  => la partie est fini*)
+  if(!autonomie <= 0) then 
+			begin 
+			print_string "\n\n****** Dommage vous avez pérdu :( ******\n\n";
+			print_string "\n\n** La partie est fini, merci d'avoir joué **\n\n";
+			Graphics.close_graph ();
+			end ;
+  (* tester si le joueur a pris toute les étoiles => la partie est fini*)
+  if(!score = 120 & !autonomie > 0) then 
+			begin 
+			print_string "\n\n******* BRAVO VOUS AVEZ GAGNE :) ******\n\n";
+			print_string "\n\n** La partie est fini, merci d'avoir joué **\n\n";
+			Graphics.close_graph ();
+			end ;
+
+  print_string "Votre score : ";
+  print_int !score;
+  print_string "    Niveau d'autonomie : ";
+  print_int !autonomie;
+  print_string " %";
+  print_string "\n\n";	
   afficher_decor ();
   afficher_mobiles ();;
 
